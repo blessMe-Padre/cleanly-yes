775c4edf46561fe0d0bb1c40cfbf480731474b18
Сергей Субботин <zarodiny@yandex.ru>
2023-07-20 16:14:51 +1000

    feat: добавляет стили

--- source/js/modules/intro-slider.js
+++ source/js/modules/intro-slider.js
@@ -2,14 +2,17 @@ const initIntroSlider = () => {
     const swiper = new Swiper('.intro__swiper', {
         // Optional parameters
         loop: true,
+        speed: 2000,
         slidesPerView: 1,
+        autoplay: {
+            delay: 5000,
+        },
 
         // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-
     });
 };
 
